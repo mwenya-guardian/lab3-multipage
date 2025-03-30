69baa6dadc090192783ec4a6d69353abb3e087ba
mwenya_guardian <mwenyaguardianmg@gmail.com>
2025-03-30 18:33:53 +0000

    Created js file to along shared navbar

---
+++ script.js
@@ -0,0 +1,16 @@
+//Adding an on click event listener for UI items
+document.getElementById("navbar-container").addEventListener("click", function(event) {
+  console.log(`Clicked ${event.target}`);
+});
+//Getting nav bar from files dir
+fetch("./navbar.html")
+  .then(response =>  response.text)
+  .then((data)=>{
+    document.getElementById("navbar-container").innerHTML = data;
+  })
+  .catch(
+    (error)=>{
+      console.log(error);
+      document.getElementById("navbar-container").innerHTML = "Nav Bar Not Found(404)";
+    }
+  )
